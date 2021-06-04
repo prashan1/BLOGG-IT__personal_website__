20a320b5923c23f1543bd5a2ae3e70e7f93df453
Prashant Bisht <57286404+prashan1@users.noreply.github.com>
2021-06-04 19:20:21 +0530

    Delete blogg_it directory

--- blogg_it/dashboard/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms 
-from .models import Posts, Threads
-
-class PostForm(forms.ModelForm):
-	class Meta:
-		model = Posts
-		fields = ['heading','description'] 
--- blogg_it/user/forms.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.models import User
-from django import forms
-
-class UserDiscription(forms.ModelForm):
-	email= forms.EmailField()
-
-	class Meta:
-		models = User
-		fields = ['username','email']
